e1e2daf9f5dc06876fb444e643076330df819dbd
Shahid Nawaz <shahid_nawaz_intern@netixsol.com>
2023-11-23 23:00:09 -0800

    these three variable types  var , let and const . the data types are number , string , bool , array and object .  in this file i get variable a and b , then perform operations arithimatic , logical , comparision  on data a and b,

--- week2/day1/index.js
+++ week2/day1/index.js
@@ -1,1 +1,31 @@
 alert("Shahid nawaz")
+var a = 5;
+console.log(`valu of a is : ${a}`)
+var b = 10;
+console.log(`value of b is : ${b}`)
+
+var sum = a + b; // addition
+console.log(`sum of ${a} and ${b} is : ${sum}`)
+var diff = a - b; // subtraction
+console.log(`diff of ${a} and ${b} is :${diff}`)
+var prod = a * b; // multiplication
+console.log(`mul of ${a} and ${b} is :${prod}`)
+var quotient = a / b; // division
+console.log(`division of ${a} and ${b} is :${quotient}`)
+
+var isEqual = a === b; // equality comparison
+console.log(`euqlity camparison of ${a} and ${b} is :${isEqual}`)
+var isNotEqual = a !== b; // inequality comparison
+console.log(`inequality camparison of ${a} and ${b} is :${isNotEqual}`)
+var isGreater = a > b; // greater than comparison
+console.log(`greater than copmarison of ${a} and ${b} is :${isGreater}`)
+var isLess = a < b; // less than comparison
+console.log(`less than copmarison of ${a} and ${b} is :${isLess}`)
+
+var logicalAnd = a < 10 && b > 5; // logical AND
+console.log(`logical AND of ${a} and ${b} is :${logicalAnd}`)
+var logicalOr = a < 10 || b > 5; // logical OR
+console.log(`logical OR of ${a} and ${b} is :${logicalOr}`)
+
+a += 5; // shorthand for a = a + 5; (assignment)
+console.log(`${a}`)
