bc5f6598f7f7418b65f2a00a56c19884bde7c84b
Shahid Nawaz <shahid_nawaz_intern@netixsol.com>
2023-12-07 01:39:25 -0800

    day5 is completed

--- week3/day5/app.js
+++ week3/day5/app.js
@@ -1,6 +1,26 @@
 const express = require("express")
 const bodyParser=require("body-parser")
+const ejs = require('ejs')
 const app = express();
+app.set('view engine', 'ejs');
  app.use(bodyParser.urlencoded({extended:true}))
- app.get('/',(req, res)=>{res.send("shahid is is response i send you")})
+ var items=[]
+ app.get('/',(req, res)=>{
+    // let today=new Date();
+    // console.log(today)
+    // let day = today.getDay();
+    // console.log(day)
+    // let time= today.getTime()
+    // console.log(time)
+    let options = { weekday: 'long', year: 'numeric', month: 'long', day: 'numeric' };
+    let today  = new Date();
+    let day=today.toLocaleDateString("en-US", options)
+    res.render("index",{today:day , newItemList:items})
+})
+app.post("/",(req, res)=>{
+   let item=req.body.newItem
+   items.push(item)
+    res.redirect("/")
+
+})
  app.listen(3000,()=>{console.log("app is listen")})
