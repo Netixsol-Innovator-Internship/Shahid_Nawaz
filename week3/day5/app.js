9e934fe6b06ab4556b34a86158bc6ebddc6c865d
Shahid Nawaz <shahid_nawaz_intern@netixsol.com>
2023-12-06 22:04:48 -0800

    i make first routed using  get

--- week3/day5/app.js
+++ week3/day5/app.js
@@ -2,4 +2,5 @@ const express = require("express")
 const bodyParser=require("body-parser")
 const app = express();
  app.use(bodyParser.urlencoded({extended:true}))
+ app.get('/',(req, res)=>{res.send("shahid is is response i send you")})
  app.listen(3000,()=>{console.log("app is listen")})
