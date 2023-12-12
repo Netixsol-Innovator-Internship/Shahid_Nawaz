436233156f9bb1a9498ac32c36922d37ce816c1e
Shahid Nawaz <shahid_nawaz_intern@netixsol.com>
2023-12-11 23:47:22 -0800

    ...

--- week3/day2/src/script.js
+++ week3/day2/src/script.js
@@ -1,21 +1,21 @@
-const input=document.querySelector("#search")
-const card=document.querySelector("#card")
-const button=document.querySelector("#btn")
+const input = document.querySelector("#search");
+const card = document.querySelector("#card");
+const button = document.querySelector("#btn");
 const githubURL = "https://api.github.com/users/";
 
 // const userName="shahid"
-const getUser=async(userName)=>{
- fetch(githubURL+userName)
-  .then(response => {
-    if (!response.ok) {
-      throw new Error(`HTTP error! Status: ${response.status}`);
-    }
-    
-    return response.json();
-  }) 
-  .then(data => {
-    console.log(data);
-    card.innerHTML=`
+const getUser = async (userName) => {
+  fetch(githubURL + userName)
+    .then((response) => {
+      if (!response.ok) {
+        throw new Error(`HTTP error! Status: ${response.status}`);
+      }
+
+      return response.json();
+    })
+    .then((data) => {
+      console.log(data);
+      card.innerHTML = `
     <div class="flex justify-center ">
                     <img class="w-32 h-32 rounded-full"
                         src="${data.avatar_url}"
@@ -24,69 +24,90 @@ const getUser=async(userName)=>{
                     <div class="lg:w-3/4 mx-auto  md:p-3 flex flex-col space-y-6">
                       <div class="flex flex-row max-sm:flex-col justify-between ">
                          <div class="flex flex-col ">
-                           <h1 id="name" class="text-lg font-bold">${data.name}</h1>
-                           <p class="text-blue-300">${data.company==""?data.company:"not available"}</p>
+                           <h1 id="name" class="text-lg font-bold">${
+                             data.name
+                           }</h1>
+                           <p class="text-blue-300">${
+                             data.login != null ? data.login : "not available"
+                           }</p>
                          </div>
                          <div class="">
-                            <h1 class="font-bold">${data.created_at.split('T')[0]}</h1>
+                            <h1 class="font-bold">${
+                              data.created_at.split("T")[0]
+                            }</h1>
                          </div>
                       </div>
-                      <h1 class="">${data.bio==""?data.bio:""}</h1>
+                      <h1 class="">${
+                        data.bio != null ? data.bio : "Not available"
+                      }</h1>
                     <div class="flex flex-row items-center justify-between bg-slate-100 rounded-lg shadow-md md:p-7 dark:bg-slate-700">
                             <div class="flex flex-col items-center justify-center">
                                 <h1 class="text-lg max-sm:text-sm max-sm:font-medium font-semibold pb-6">Repoes</h1>
-                                <p class="font-extrabold text-3xl">${data.public_repos}</p>
+                                <p class="font-extrabold text-3xl">${
+                                  data.public_repos
+                                }</p>
                              </div>
                             <div class="flex flex-col items-center justify-center">
                                 <h1 class="text-lg font-semibold pb-6">Follower</h1>
-                                <p class="font-extrabold text-3xl">${data.followers}</p>
+                                <p class="font-extrabold text-3xl">${
+                                  data.followers
+                                }</p>
                              </div>
                              <div class="flex flex-col items-center justify-center">
                                 <h1 class="text-lg font-semibold pb-6">Following</h1>
-                                <p class="font-extrabold text-3xl">${data.following}</p>
+                                <p class="font-extrabold text-3xl">${
+                                  data.following
+                                }</p>
                              </div>
                         </div>
                         <div class="flex flex-row max-sm:flex-col items-start flex-nowrap mt-8 ">
                             <div class="flex flex-row w-1/2 space-x-4">
                                 <span><ion-icon name="location-sharp"></ion-icon></span>
-                                <p>${data.location==""?data.location:"Not available"}</p>
+                                <p>${
+                                  data.location != null
+                                    ? data.location
+                                    : "Not available"
+                                }</p>
                             </div>
                             <div class="flex flex-row w-1/2 space-x-4">
                                 <span><ion-icon name="logo-twitter"></ion-icon></span>
-                                <p>${data.twitter_username==""?data.twitter_username:"not available"}</p>
+                                <p>${
+                                  data.twitter_username != null
+                                    ? data.twitter_username
+                                    : "not available"
+                                }</p>
                             </div>
                         </div>
                         <div class="flex flex-row max-sm:flex-col items-start flex-nowrap mt-8">
                             <div class="flex flex-row w-1/2 space-x-4">
                                 <span><ion-icon name="link-sharp"></ion-icon></span>
-                                <p>${data.html_url
-                                  ==""?data.html_url
-                                  :"not available"}</p>
+                                <p>${
+                                  data.blog != ""
+                                    ? data.html_url
+                                    : "not available"
+                                }</p>
                             </div>
                             <div class="flex flex-row w-1/2 space-x-4">
                                 <span><ion-icon name="business-sharp"></ion-icon></span>
                                 <p>@github</p>
                             </div>
                     </div>
-    `
-  })
-  .catch(error => {
-    console.error("Error:", error);
-  });
- 
-}
+    `;
+    })
+    .catch((error) => {
+      console.error("Error:", error);
+    });
+};
 // getUser("ali")
 // button.addEventListener("click",function(){
 //   console.log(input.value)
 // getUser(input.value)
 // })
 
-
-function get(){
-  console.log(input.value)
-  getUser(input.value)
+function get() {
+  console.log(input.value);
+  getUser(input.value);
 }
-
 
 // // dark mode script start
 // const darkButton = document.getElementById("darkButton");
--- week3/day5/task1/app.js
+++ week3/day5/task1/app.js
@@ -3,7 +3,8 @@ const bodyParser=require("body-parser")
 const ejs = require('ejs')
 const app = express();
 app.set('view engine', 'ejs');
- app.use(bodyParser.urlencoded({extended:true}))
+app.use(bodyParser.urlencoded({extended:true}))
+app.use(express.json());
  var items=[]
  app.get('/',(req, res)=>{
     // let today=new Date();
@@ -32,10 +33,28 @@ app.delete('/delete/:index', (req, res) => {
 // ... (your existing code)
 
 app.post("/",(req, res)=>{
+    console.log("posrrbody ",req.body);
    let item=req.body.newItem
    items.push(item)
     res.redirect("/")
 })
+
+app.put('/edit/:index', (req, res) => {
+    console.log("reqbodyyy", req.body);
+    
+    const index = req.params.index;
+    console.log(index)
+    if (index >= 0 && index < items.length) {
+        console.log(req.body);
+        const newItem = req.body.editedItem;
+        console.log(newItem);
+        items[index] = newItem;
+        console.log(items[index]);
+        res.json({ success: true });
+    } else {
+        res.json({ success: false });
+    }
+});
 
 
  app.listen(3000,()=>{console.log("app is listen")})
